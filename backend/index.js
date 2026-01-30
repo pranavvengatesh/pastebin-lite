3280c2b592efdf8384954324b071548d81a2893e
pranavvengatesh <146944225+pranavvengatesh@users.noreply.github.com>
2026-01-30 23:02:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import pasteRoutes from "./routes/pasteroutes.js";
+import pasteRoutes from "./routes/pasteRoutes.js";
 dotenv.config();
 
 const app = express();
